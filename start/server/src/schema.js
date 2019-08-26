22893b09b9419e2c2c1f0a1abe41ce45cc8a7212
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-26 14:42:12 +0300

    add dadasources

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,57 +1,56 @@
 const { gql } = require('apollo-server');
 
-
 const typeDefs = gql`
-    type Launch {
-        id: ID!
-        site: String
-        mission: Mission
-        rocket: Rocket
-        isBooked: Boolean!
-    }
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
 
-    type Rocket {
-        id: ID!
-        name: String
-        type: String
-    }
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
 
-    type User {
-        id: ID!
-        email: String!
-        trips: [Launch]!
-    }
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
 
-    type Mission {
-        name: String
-        missionPatch(size: PatchSize): String
-    }
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
 
-    enum PatchSize {
-        SMALL
-        LARGE
-    }
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
 
-    type TripUpdateResponse {
-        success: Boolean!
-        message: String
-        launches: [Launch]
-    }
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
 
-    type Mutation {
-        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancelTrip(launchId: ID!): TripUpdateResponse!
 
-        cancelTrip(launchId: ID!): TripUpdateResponse!
+    login(email: String): String
+  }
 
-        login(email: String): String
-    }
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
 
-    type Query {
-        launches: [Launch]!
-        launch(id: ID!): Launch
-        # Queries for the current user
-        me: User
-    }
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    # Queries for the current user
+    me: User
+  }
 `;
 
 module.exports = typeDefs;
